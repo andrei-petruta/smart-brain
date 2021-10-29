ea2967292af8414d8e8c454a9c62ad61e9f4bb79
Andrei Petruta <aandrei.petruta@gmail.com>
2021-10-29 15:10:09 +0200

    link react app to backend on heroku

--- src/App.js
+++ src/App.js
@@ -147,7 +147,7 @@ class App extends React.Component {
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    fetch("http://localhost:3001/imageurl", {
+    fetch("https://blooming-journey-87644.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -157,7 +157,7 @@ class App extends React.Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3001/image", {
+          fetch("https://blooming-journey-87644.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
